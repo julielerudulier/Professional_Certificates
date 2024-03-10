b215ac6453894f90fd97f99c5c57b2640a09580f
Julie Le Rudulier <136101496+julielerudulier@users.noreply.github.com>
2024-03-10 21:32:19 +0100

    Create Node.java

---
+++ Introduction to Object-Oriented Programming with Java/Part III: Exceptions, Data Structures, Recursion, and GUIs/HW07 - LinkedList/Node.java
@@ -0,0 +1,29 @@
+public class Node<T> {
+    private T data;
+    private Node<T> next;
+
+    public Node(T data, Node<T> next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    public Node(T data) {
+        this(data, null);
+    }
+
+    public T getData() {
+        return this.data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Node<T> getNext() {
+        return this.next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+}
