8370a7396414071db08145334e8b0b156293da82
Julie Le Rudulier <136101496+julielerudulier@users.noreply.github.com>
2024-03-11 15:04:37 +0100

    Create MyEmotions.py

---
+++ Introduction to Python Programming/Part I: Fundamentals and Procedural Programming/Chapter 2 - Coding Problems/MyEmotions.py
@@ -0,0 +1,46 @@
+busy = True
+hungry = False
+tired = True
+stressed = False
+
+#You may modify the lines of code above, but don't move them!
+#When you Submit your code, we'll change these lines to
+#assign different values to the variables.
+
+#Logical operators get more complex when we start using them
+#with the results of other logical operators. So, let's try
+#it out!
+#
+#Using the variables above, we want to assess whether the
+#person is happy, sad, bored, confused, or anxious.
+#
+# - The person is happy if they're busy but not stressed.
+# - The person is sad if they're either hungry or tired.
+# - The person is confused if they're both happy and sad.
+# - The person is bored if they're neither happy, sad,
+#   nor busy.
+# - The person is anxious if they're neither happy nor sad,
+#   but they are stressed.
+#
+#Add code below whose output will list whether each of these
+#emotions is true or false. For example, with the original
+#values of the variables above, this would print:
+#
+#Happy: True
+#Sad: True
+#Confused: True
+#Bored: False
+#Anxious: False
+
+
+#Add your code here!
+happy = busy and not stressed
+sad = hungry or tired
+
+print("Happy:", happy)
+print("Sad:", sad)
+print("Confused:", happy and sad)
+print("Bored:", not (happy or sad or busy))
+print("Anxious:", not (happy or sad) and stressed)
+
+
