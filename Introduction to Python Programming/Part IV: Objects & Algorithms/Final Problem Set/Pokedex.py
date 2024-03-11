23b025679193653e87abd3ab600be363b916d7a6
Julie Le Rudulier <136101496+julielerudulier@users.noreply.github.com>
2024-03-11 16:46:52 +0100

    Create Pokedex.py

---
+++ Introduction to Python Programming/Part IV: Objects & Algorithms/Final Problem Set/Pokedex.py
@@ -0,0 +1,54 @@
+#The line below will open a file containing information
+#about every pokemon through Generation 7:
+
+file = open('../resource/lib/public/pokedex.csv', 'r')
+
+#We've also provided a sample subset of the data in
+#sample.csv.
+#
+#Each line of the file has 13 values, separated by commas.
+#They are: 
+#
+# - Number: The numbered ID of the Pokemon, an integer
+# - Name: The name of the Pokemon, a string
+# - Type1: The Pokemon's primary type, a string
+# - Type2: The Pokemon's secondary type, a string (this
+#   may be blank)
+# - HP: The Pokemon's HP statistic, an integer in the range
+#   1 to 255
+# - Attack: The Pokemon's Attack statistic, an integer in
+#   the range 1 to 255
+# - Defense: The Pokemon's Defense statistic, an integer in
+#   the range 1 to 255
+# - SpecialAtk: The Pokemon's Special Attack statistic, an
+#   integer in the range 1 to 255
+# - SpecialDef: The Pokemon's Special Defense statistic, an
+#   integer in the range 1 to 255
+# - Speed: The Pokemon's Speed statistic, an integer in the
+#   range 1 to 255
+# - Generation: What generation the Pokemon debuted in, an
+#   integer in the range 1 to 7
+# - Legendary: Whether the Pokemon is considered "legendary"
+#   or not, either TRUE or FALSE
+# - Mega: Whether the Pokemon is "Mega" or not, either TRUE
+#   or FALSE
+#
+#Use this dataset to answer the questions below.
+
+#Here, add any code you want to allow you to answer the
+#questions asked below over on edX. This is just a sandbox
+#for you to explore the dataset: nothing is required for
+#submission here.
+
+# Answer to question 1: 420
+# Answer to question 2: Water
+# Answer to question 3: Blissey
+# Answer to question 4: Shuckle
+# Answer to question 5: Psychic
+# Answer to question 6: Cosmog
+# Answer to question 7: Slaking
+# Answer to question 8: Flying
+# Answer to question 9: 85
+# Answer to question 10: 7
+# Answer to question 11: 2
+# Answer to question 12: 100
