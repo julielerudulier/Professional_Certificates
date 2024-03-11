7e5e5475fda75236520aaf05e60af957774b9947
Julie Le Rudulier <136101496+julielerudulier@users.noreply.github.com>
2024-03-11 14:16:20 +0100

    Update ArrayList.java

--- Data Structures and Algorithms/Part I: ArrayLists, LinkedLists, Stacks and Queues/Module 1 Assignment: ArrayLists/ArrayList.java
+++ Data Structures and Algorithms/Part I: ArrayLists, LinkedLists, Stacks and Queues/Module 1 Assignment: ArrayLists/ArrayList.java
@@ -1,86 +1,80 @@
 import java.util.NoSuchElementException;
 
 public class ArrayList<T> {
-
-    public static final int INITIAL_CAPACITY = 9;
-
-    private T[] backingArray;
-    private int size;
-
-    public ArrayList() {
-        backingArray = (T[]) new Object[INITIAL_CAPACITY];
-    }
-    
-    public void addToFront(T data) {
-	if (data == null) {
-		throw new IllegalArgumentException("No data.");
-	} 
-	if (size < backingArray.length) {
-		for (int i = size - 1; i >= 0; i--) {
-			backingArray[i+1] = backingArray[i];
-		}		
-		backingArray[0] = data;
-		size += 1; 	
-	} else if (size == backingArray.length) {
-            T[] newArray = (T[]) new Object[backingArray.length * 2];
-            // newArray[0] = data;
-            for (int i = 0; i < backingArray.length; i++) {
-                newArray[i+1] = backingArray[i];
-            }
-            newArray[0] = data;
-            backingArray = newArray;
-            size += 1;
-        }
-    }
-
-    public void addToBack(T data) {
-        if (data == null) {
-		throw new IllegalArgumentException("No data.");
-	} 
-	if (size < backingArray.length) {
-		backingArray[size] = data;
-		// size++;
-		size += 1; 	
-	} else if (size == backingArray.length) {
-            T[] newArray = (T[]) new Object[backingArray.length * 2];
-            // newArray[0] = data;
-            for (int i = 0; i < backingArray.length; i++) {
-                newArray[i] = backingArray[i];
-            }
-            newArray[size] = data;
-	    size += 1;
-            backingArray = newArray;
-        }
-    }
-
-    public T removeFromFront() {
-        if (size == 0) {
-		throw new NoSuchElementException("The list is currently empty.");
-	} 
-	T data = backingArray[0];
-	for (int i = 0; i < size-1; i++) {
-		backingArray[i] = backingArray[i+1];
+	public static final int INITIAL_CAPACITY = 9;
+	private T[] backingArray;
+    	private int size;
+	public ArrayList() {
+		backingArray = (T[]) new Object[INITIAL_CAPACITY];
 	}
-	backingArray[size-1] = null;
-	size -= 1;
-	return data;
-    }
+	
+	public void addToFront(T data) {
+		if (data == null) {
+			throw new IllegalArgumentException("No data.");
+		} 
+		if (size < backingArray.length) {
+			for (int i = size - 1; i >= 0; i--) {
+				backingArray[i+1] = backingArray[i];
+			}		
+			backingArray[0] = data;
+			size += 1; 
+		} else if (size == backingArray.length) {
+			T[] newArray = (T[]) new Object[backingArray.length * 2];
+			for (int i = 0; i < backingArray.length; i++) {
+				newArray[i+1] = backingArray[i];
+			}
+			newArray[0] = data;
+			backingArray = newArray;
+			size += 1;
+		}
+	}
+
+	public void addToBack(T data) {
+		if (data == null) {
+			throw new IllegalArgumentException("No data.");
+		} 
+		if (size < backingArray.length) {
+			backingArray[size] = data;
+			size += 1; 	
+		} else if (size == backingArray.length) {
+			T[] newArray = (T[]) new Object[backingArray.length * 2];
+			for (int i = 0; i < backingArray.length; i++) {
+				newArray[i] = backingArray[i];
+			}
+			newArray[size] = data;
+			size += 1;
+			backingArray = newArray;
+		}
+	}
+	
+	public T removeFromFront() {
+		if (size == 0) {
+			throw new NoSuchElementException("The list is currently empty.");
+		} 
+		T data = backingArray[0];
+		for (int i = 0; i < size-1; i++) {
+			backingArray[i] = backingArray[i+1];
+		}
+		backingArray[size-1] = null;
+		size -= 1;
+		return data;
+	}
    
-    public T removeFromBack() {
-        if (size == 0) {
-		throw new NoSuchElementException("The list is currently empty.");
+	public T removeFromBack() {
+		if (size == 0) {
+			throw new NoSuchElementException("The list is currently empty.");
+		}
+		T data = backingArray[size-1];
+		backingArray[size-1] = null;
+		size -= 1;
+		return data;
 	}
-	T data = backingArray[size-1];
-	backingArray[size-1] = null;
-	size -= 1;
-	return data;	
-    }
 
-    public T[] getBackingArray() {
-        return backingArray;
-    }
+    	public T[] getBackingArray() {
+        	return backingArray;
+    	}
 
-    public int size() {
-        return size;
-    }
+    	public int size() {
+        	return size;
+    	}
 }
