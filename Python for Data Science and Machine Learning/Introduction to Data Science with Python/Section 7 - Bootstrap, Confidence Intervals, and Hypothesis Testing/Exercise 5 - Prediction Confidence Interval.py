8bd90240cc9e29da5f03e3f53ef9cd68347a2fe3
Julie Le Rudulier <136101496+julielerudulier@users.noreply.github.com>
2024-04-23 14:41:26 +0200

    Update Exercise 5 - Prediction Confidence Interval.py

--- Python for Data Science and Machine Learning/Introduction to Data Science with Python/Section 7 - Bootstrap, Confidence Intervals, and Hypothesis Testing/Exercise 5 - Prediction Confidence Interval.py
+++ Python for Data Science and Machine Learning/Introduction to Data Science with Python/Section 7 - Bootstrap, Confidence Intervals, and Hypothesis Testing/Exercise 5 - Prediction Confidence Interval.py
@@ -22,7 +22,7 @@ def error_func(y,y_p):
 numboot = 1000
 
 # Set the budget amount of $1000. We have used a 2d list to facilitate model prediction (sklearn.LinearRegression requires input as a 2d array)
-budget = [[1000, 1000, 1000]]
+budget = [[1000]]
 
 # Define an empty list that will store sales predictions for each bootstrap
 sales_dist = []
@@ -33,13 +33,13 @@ sales_dist = []
 for i in range(numboot):
     # Bootstrap using df.sample method.
     df_new = df.sample(frac=1, replace=True)
-    x = df_new[['TV', 'Radio', 'Newspaper']].values
+    x = df_new[['TV']].values
     y = df_new['Sales'].values
     linreg = LinearRegression()
     linreg.fit(x,y)
     prediction = linreg.predict(budget)
     y_pred = linreg.predict(x) 
-    error = np.random.normal(0,error_func(y,y_pred))
+    error = np.random.normal(0,error_func(y, y_pred))
   
     # The final sales prediction is the sum of the model prediction and the error term
     sales = prediction + error
@@ -50,11 +50,11 @@ for i in range(numboot):
 sales_dist.sort()
 
 # find the 95% confidence interval using np.percentile function at 2.5% and 97.5%
-sales_CI = (np.percentile(sales_dist,2.5),np.percentile(sales_dist, 97.5))
+sales_CI = (np.percentile(sales_dist, 2.5), np.percentile(sales_dist, 97.5))
 
 # Use this helper function to plot the histogram of beta values along with the 95% confidence interval
 def plot_simulation(simulation,confidence):
-    plt.hist(simulation, bins = 30, label = 'beta distribution', align = 'left', density = True,edgecolor='k')
+    plt.hist(simulation, bins = 30, label = 'beta distribution', align = 'left', density = True, edgecolor='k')
     plt.axvline(confidence[1], 0, 1, color = 'r', label = 'Right Interval')
     plt.axvline(confidence[0], 0, 1, color = 'red', label = 'Left Interval')
     plt.xlabel('Beta value')
@@ -62,7 +62,7 @@ def plot_simulation(simulation,confidence):
     plt.legend(frameon = False, loc = 'upper right')
 
 # call the function above with the computed sales distribution and the confidence intervals from earlier
-plot_simulation(sales_dist,sales_CI)
+plot_simulation(sales_dist, sales_CI)
 
 # Print the computed values
 print(f"With a TV advertising budget of ${budget[0][0]},")
